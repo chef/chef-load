fce1a9ae14bdf9e7dd69c8f6f7c4eb47a1536104
Jeremiah Snapp <jeremiah@chef.io>
2016-10-20 15:08:11 -0400

    Reorganizing code in cookbooks.go

--- cookbooks.go
+++ cookbooks.go
@@ -7,6 +7,40 @@ import (
 	"github.com/go-chef/chef"
 )
 
+type cookbookFile struct {
+	Checksum    string `json:"checksum"`
+	Name        string `json:"name"`
+	Path        string `json:"path"`
+	Specificity string `json:"specificity"`
+	URL         string `json:"url"`
+}
+
+type cookbook struct {
+	CookbookName string         `json:"cookbook_name"`
+	Name         string         `json:"name"`
+	Version      string         `json:"version"`
+	Attributes   []cookbookFile `json:"attributes"`
+	Definitions  []cookbookFile `json:"definitions"`
+	Files        []cookbookFile `json:"files"`
+	Libraries    []cookbookFile `json:"libraries"`
+	Providers    []cookbookFile `json:"providers"`
+	Recipes      []cookbookFile `json:"recipes"`
+	Resources    []cookbookFile `json:"resources"`
+	RootFiles    []cookbookFile `json:"root_files"`
+	Templates    []cookbookFile `json:"templates"`
+}
+
+func getCookbookFile(nodeClient *chef.Client, cookbookFile string) {
+	req, err := nodeClient.NewRequest("GET", cookbookFile, nil)
+	res, err := nodeClient.Do(req, nil)
+	if err != nil {
+		fmt.Println(err)
+		// TODO: need to handle errors better
+		return
+	}
+	defer res.Body.Close()
+}
+
 func downloadCookbooks(nodeClient *chef.Client, cookbooks map[string]json.RawMessage) {
 	for _, v := range cookbooks {
 		var cookbook cookbook
@@ -40,38 +74,4 @@ func downloadCookbooks(nodeClient *chef.Client, cookbooks map[string]json.RawMes
 			getCookbookFile(nodeClient, cookbookFile.URL)
 		}
 	}
-}
-
-type cookbook struct {
-	CookbookName string         `json:"cookbook_name"`
-	Name         string         `json:"name"`
-	Version      string         `json:"version"`
-	Attributes   []cookbookFile `json:"attributes"`
-	Definitions  []cookbookFile `json:"definitions"`
-	Files        []cookbookFile `json:"files"`
-	Libraries    []cookbookFile `json:"libraries"`
-	Providers    []cookbookFile `json:"providers"`
-	Recipes      []cookbookFile `json:"recipes"`
-	Resources    []cookbookFile `json:"resources"`
-	RootFiles    []cookbookFile `json:"root_files"`
-	Templates    []cookbookFile `json:"templates"`
-}
-
-type cookbookFile struct {
-	Checksum    string `json:"checksum"`
-	Name        string `json:"name"`
-	Path        string `json:"path"`
-	Specificity string `json:"specificity"`
-	URL         string `json:"url"`
-}
-
-func getCookbookFile(nodeClient *chef.Client, cookbookFile string) {
-	req, err := nodeClient.NewRequest("GET", cookbookFile, nil)
-	res, err := nodeClient.Do(req, nil)
-	if err != nil {
-		fmt.Println(err)
-		// TODO: need to handle errors better
-		return
-	}
-	defer res.Body.Close()
 }
