1d890b38679bdf3d022cf8d866af6682001c4622
Jeremiah Snapp <jeremiah@chef.io>
2017-08-31 11:10:37 -0400

    Update habitat plan for 3.0.0

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,19 +1,19 @@
 pkg_name=chef-load
 pkg_origin=jeremiahsnapp
-pkg_version="2.2.0"
+pkg_version="3.0.0"
 pkg_license=('UNLICENSED')
 # This plan does not build the package from source, but instead downloads the
 # statically linked binary associated with the $pkg_version from GitHub
 # releases.
-pkg_source="https://github.com/jeremiahsnapp/$pkg_name/releases/download/v$pkg_version/${pkg_name}_${pkg_version}_Linux_64bit"
-pkg_shasum="54741315aeaf0aea9c444f3e8a22e1b312fece7e478c440463f98264dced9a70"
+pkg_source="https://github.com/chef/$pkg_name/releases/download/v$pkg_version/${pkg_name}_${pkg_version}_Linux_64bit"
+pkg_shasum="0ad6612192b9bb00089591e6a36d937b14e465500ca721be778e8ab81ac617f1"
 pkg_bin_dirs=(bin)
 pkg_binds_optional=(
   [automate]="port"
   [chef-server]="port"
 )
 pkg_description="A tool that simulates Chef Client API load on a Chef Server and/or a Chef Automate server"
-pkg_upstream_url="https://github.com/jeremiahsnapp/chef-load"
+pkg_upstream_url="https://github.com/chef/chef-load"
 
 do_unpack() {
   return 0
