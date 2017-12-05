92bee0efd0ca31425740e8460f91a1093080ab3d
Salim Afiune <afiune@chef.io>
2017-12-05 11:30:57 -0500

    Update plan.sh to build chef-load from src
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,34 +1,19 @@
 pkg_name=chef-load
 pkg_origin=chef
-pkg_version="3.0.0"
+pkg_version="4.0.0"
 pkg_license=('UNLICENSED')
-# This plan does not build the package from source, but instead downloads the
-# statically linked binary associated with the $pkg_version from GitHub
-# releases.
-pkg_source="https://github.com/chef/$pkg_name/releases/download/v$pkg_version/${pkg_name}_${pkg_version}_Linux_64bit"
-pkg_shasum="0ad6612192b9bb00089591e6a36d937b14e465500ca721be778e8ab81ac617f1"
+pkg_description="A tool that simulates Chef Client API load on a Chef Server and/or a Chef Automate server"
+pkg_upstream_url="https://github.com/chef/chef-load"
 pkg_bin_dirs=(bin)
 pkg_binds_optional=(
   [automate]="port"
   [chef-server]="port"
 )
-pkg_description="A tool that simulates Chef Client API load on a Chef Server and/or a Chef Automate server"
-pkg_upstream_url="https://github.com/chef/chef-load"
-
-do_unpack() {
-  return 0
-}
-
-do_build() {
-  return 0
-}
-
-do_install() {
-  install -v -m 0755 \
-    "$HAB_CACHE_SRC_PATH/${pkg_name}_${pkg_version}_Linux_64bit" \
-    "$pkg_prefix/bin/$pkg_name"
-}
-
-do_strip() {
-  return 0
-}
+pkg_scaffolding=core/scaffolding-go
+scaffolding_go_base_path=github.com/chef
+scaffolding_go_build_deps=(
+ github.com/go-chef/chef
+ github.com/naoina/toml
+ github.com/satori/go.uuid
+ github.com/sirupsen/logrus
+)
