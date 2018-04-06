9733b40999338e549e96f4bfc4fcffc8a4d9a9b1
Stephan Renatus <srenatus@chef.io>
2018-04-06 13:52:26 +0200

    habitat: use core/dep in plan.sh
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -5,6 +5,7 @@ pkg_license=('Apache-2.0')
 pkg_description="A tool that simulates Chef Client API load on a Chef Server and/or a Chef Automate server"
 pkg_upstream_url="https://github.com/chef/chef-load"
 pkg_bin_dirs=(bin)
+pkg_build_deps=(core/dep)
 pkg_deps=(core/glibc)
 pkg_binds_optional=(
   [automate]="port"
@@ -12,10 +13,12 @@ pkg_binds_optional=(
 )
 pkg_scaffolding=core/scaffolding-go
 scaffolding_go_base_path=github.com/chef
-scaffolding_go_build_deps=(
- github.com/go-chef/chef
- github.com/satori/go.uuid
- github.com/sirupsen/logrus
- github.com/spf13/cobra
- github.com/spf13/viper
-)
+scaffolding_go_build_deps=()
+
+do_download(){
+  export scaffolding_go_pkg_path="$scaffolding_go_workspace_src/$scaffolding_go_base_path/$pkg_name"
+  build_line "Getting Go dependencies (dep ensure)"
+  pushd $scaffolding_go_pkg_path >/dev/null
+    dep ensure
+  popd >/dev/null
+}
